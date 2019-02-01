e38597b71b7378822729e6e75348825b699301a3
Team Herman Miller <cse498@CSE498s-iMac-4.local>
2019-02-01 12:54:50 -0500

    complete class made

--- PlacenoteSDKExample/NavigationNode.swift
+++ PlacenoteSDKExample/NavigationNode.swift
@@ -11,11 +11,57 @@ import SceneKit
 
 
 class NavigationNode {
-    var type: Double
+    var type: ShapeType
+    var nodeNum: Double
     var position: SCNVector3
-    init(type: Double, position: SCNVector3){
-        self.type     = type
+    var node: SCNNode
+    init(number: Double, Stype: ShapeType, position: SCNVector3){
+        self.type     = Stype
         self.position = position
+        self.node     = SCNNode()
+        self.nodeNum  = number
+
     }
+    func getNode() -> SCNNode{
+        switch self.nodeNum {
+        case 1:
+            return generateBreadCrumb(pos01: self.position)
+        case 2:
+            return generateCheckpoint(vector_pos: self.position)
+        case 3:
+            return generateDestination(pos01: self.position)
+        default:
+            return SCNNode()
+        }
+    }
+    
+    func generateBreadCrumb(pos01: SCNVector3) -> SCNNode{
+        let geometry:SCNGeometry = SCNSphere(radius: 0.1) //meters
+        geometry.materials.first?.diffuse.contents = UIColor.red
+        let geometryNode = SCNNode(geometry: geometry)
+        geometryNode.position = pos01
+        geometryNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        return geometryNode
+    }
+    
+    func generateCheckpoint(vector_pos: SCNVector3) -> SCNNode{
+        let geometry: SCNGeometry = SCNBox(width: 0.5, height: 0.3, length: 0.8, chamferRadius: 1)
+        geometry.materials.first?.diffuse.contents = UIColor.red
+        let checkpointNode = SCNNode(geometry: geometry)
+        checkpointNode.position = vector_pos
+        checkpointNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        return checkpointNode
+    }
+    
+    func generateDestination(pos01: SCNVector3) -> SCNNode{
+        let geometry:SCNGeometry = SCNPyramid(width: 0.5, height: 1, length: 0.7) //meters
+        geometry.materials.first?.diffuse.contents = UIColor.red
+        let geometryNode = SCNNode(geometry: geometry)
+        geometryNode.position = pos01
+        geometryNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        return geometryNode
+    }
+    
+    // add function here to delete node from shapeNodes currently has a bug where the shapes don't get deleted
     
 }
