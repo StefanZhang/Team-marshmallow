9a32370d3d2515abe1ffcb64d0d231dbbae7613d
John Riley <johnriley@johns-macbook-pro.local>
2019-02-13 17:10:43 -0500

    Search bar works with tableview

--- PlacenoteSDKExample/ViewControllerWT.swift
+++ PlacenoteSDKExample/ViewControllerWT.swift
@@ -10,28 +10,48 @@ import UIKit
 
 var Destination_array = [String]() // Store Destination Name
 
-class ViewControllerWT: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewControllerWT: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var tempArray = ["Aaron", "John", "Matthew", "Stefan", "Zhenru"]
+    var search = [String]()
+    var searching = false
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tempArray.count
+        if searching{
+            return search.count
+        }
+        else{
+            return tempArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        cell?.textLabel?.text = tempArray[indexPath.row]
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cell")
+        if searching{
+            cell?.textLabel?.text = search[indexPath.row]
+        }
+        else{
+            cell?.textLabel?.text = tempArray[indexPath.row]
+        }
         return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String){
+        search = tempArray.filter({$0.prefix(searchText.count) == searchText})
+        searching = true
+        self.tableView.reloadData()
     }
 
 }
