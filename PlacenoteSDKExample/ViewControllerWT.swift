b8e4a75adfcb48057cfef7e34b371baadb2e7ad3
John Riley <johnriley@johns-macbook-pro.local>
2019-02-13 11:35:13 -0500

    working table view on where to?

--- PlacenoteSDKExample/ViewControllerWT.swift
+++ PlacenoteSDKExample/ViewControllerWT.swift
@@ -10,23 +10,28 @@ import UIKit
 
 var Destination_array = [String]() // Store Destination Name
 
-class ViewControllerWT: UIViewController {
-
+class ViewControllerWT: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var tempArray = ["Aaron", "John", "Matthew", "Stefan", "Zhenru"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tempArray.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = tempArray[indexPath.row]
+        return cell!
+    }
 
 }
