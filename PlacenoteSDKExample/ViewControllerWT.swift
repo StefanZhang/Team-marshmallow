53313ef6d0a77d3d213f1e75b0c195f5f0ad3347
Stefan Zhang <zhang661@msu.edu>
2019-03-17 23:45:11 -0400

    1. Clean files 2. Display the nav bar

--- PlacenoteSDKExample/ViewControllerWT.swift
+++ PlacenoteSDKExample/ViewControllerWT.swift
@@ -33,13 +33,13 @@ class ViewControllerWT: UIViewController, UITableViewDelegate, UITableViewDataSo
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(true, animated: animated)
+        //super.viewWillAppear(animated)
+        //navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: animated)
+        //super.viewWillDisappear(animated)
+        //navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
     
