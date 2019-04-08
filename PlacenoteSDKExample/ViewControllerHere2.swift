6361ed92545796fd4725823448b1e9f70a274cf4
Team Marshmallow <marshmallowteam234@outlook.com>
2019-04-08 17:23:42 -0400

    you are here screen

---
+++ PlacenoteSDKExample/ViewControllerHere2.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerHere2.swift
+//  Where To?
+//
+//  Created by Team Herman Miller on 4/8/19.
+//  Copyright © 2019 Vertical. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerHere2: UIViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "backgroundBlur6")!)
+
+    }
+
+}
